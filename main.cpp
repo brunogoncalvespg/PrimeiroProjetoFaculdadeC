2ebeefd2857dbe776da507b28124903fe1f6bdda
Bruno Goncalves <brunogoncalvespg@icloud.com>
2021-11-08 10:05:28 -0300

    Projeto em C
    Primeiro exercício da faculdade na poderosa linguagem C, programa simples que captura e printa dados.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char** argv) {
+	
+	
+	char nome [30];
+	char endereco [30];
+	int idade;
+	
+	
+	printf("Digite seu nome \n");
+	scanf("%s", &nome);
+
+	printf("Digite seu endereco \n");
+	scanf("%s", &endereco);
+	
+	printf("Digite sua idade \n");
+	scanf("%d", &idade);
+	
+	printf("\n nome: %s", nome);
+	printf("\n nome: %s", endereco);
+	printf("\n idade: %d", idade);
+	
+	system("pause");
+	
+	return 0;
+}
